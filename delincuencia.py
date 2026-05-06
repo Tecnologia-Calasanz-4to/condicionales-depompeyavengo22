b1ddc912b0e4bd8072bbcc6efe1b68397708616e
depompeyavengo22 <joaquemero09@gmail.com>
2026-05-06 13:16:10 -0300

    Add files via upload

---
+++ delincuencia.py
@@ -0,0 +1,8 @@
+Temp= int(input ("Decime la temperatura"))
+if Temp>25:
+    print("QUE CALOOOOOOOR OOOEEOOOOO Q CALOR Q TENGO YO Q LEVANTE LA MANO COMO YOOOOO EL QUIERE UN VINO EN CORTON")
+elif Temp<10:
+    print("HACE FRIO PA CHOMBA")
+else:
+    print("temperatura agradable")
+    
